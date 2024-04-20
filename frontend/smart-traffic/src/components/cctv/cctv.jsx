65dd5d84e02859d55b2954e2be12189feadbd81c
jmd0711 <2jmd0711@gmail.com>
2024-04-19 21:33:59 -0700

    CCTV base page

---
+++ frontend/smart-traffic/src/components/cctv/cctv.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import { Component } from "react";
+import { withRouter } from "../withrouter";
+
+class CCTV extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+   return (
+     <div className="page-container">
+       <h3 className="page-header"> CCTV PAGE </h3>
+      </div>
+   );
+  }
+}
+
+export default withRouter(CCTV);
