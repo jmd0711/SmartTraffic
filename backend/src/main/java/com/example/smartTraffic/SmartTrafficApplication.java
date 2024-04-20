ee83c268aa000b1cb462b7fce797a6d9e18e448f
anamatxcova <anamatxcova@gmail.com>
2024-04-20 13:33:24 -0700

    init backend

---
+++ backend/src/main/java/com/example/smartTraffic/SmartTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartTraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTrafficApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/smartTraffic/SmartTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartTraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
