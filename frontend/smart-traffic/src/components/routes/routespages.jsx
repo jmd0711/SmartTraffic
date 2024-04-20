d7a955befd0b03e412322fcfcae4388131a17895
jmd0711 <2jmd0711@gmail.com>
2024-04-19 21:57:01 -0700

    routes to drone and iot page

--- frontend/smart-traffic/src/components/routes/routespages.jsx
+++ frontend/smart-traffic/src/components/routes/routespages.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom"
 import Dashboard from "../dashboard/dashboard"
 import CCTV from "../cctv/cctv"
+import Drone from "../drone/drone"
+import IoT from "../iot/iot"
 import PrivateRoutes from "./privateroutes"
 
 class RoutePages extends Component {
@@ -11,6 +13,8 @@ class RoutePages extends Component {
         <Route path="/" element={<Dashboard />}/>
         <Route element={<PrivateRoutes />}>
           <Route path="/cctv" element={<CCTV />}/>
+          <Route path="/drone" element={<Drone />}/>
+          <Route path="/iot" element={<IoT />}/>
         </Route>
         <Route
           path="*"
