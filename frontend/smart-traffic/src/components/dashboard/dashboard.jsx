d64bcd363ef59dba886b5d759d1be035e59537fe
jmd0711 <2jmd0711@gmail.com>
2024-04-20 15:11:28 -0700

    dashboard layout

--- frontend/smart-traffic/src/components/dashboard/dashboard.jsx
+++ frontend/smart-traffic/src/components/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { Container, Row, Col } from "react-bootstrap"
 import { Component } from "react"
 import { withRouter } from "../withrouter"
 
@@ -9,10 +10,39 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className="page-container">
-        <h3 className="page-header"> DASHBOARD PAGE </h3>
+      <Container fluid className="page-container">
+        {/*<h3 className="page-header"> DASHBOARD PAGE </h3>*/}
+        <Row>
+          <Col xs={3} id="sidebar-wrapper">
+            <Row>
+              Issue 1
+            </Row>
+            <Row>
+              Issue 2
+            </Row>
+            <Row>
+              Issue 3
+            </Row>
+          </Col>
+          <Col xs={9}>
+            <Row>
+              Map
+            </Row>
+            <Row>
+              <Col>
+                Stat1
+              </Col>
+              <Col>
+                Stat2
+              </Col>
+              <Col>
+                Stat3
+              </Col>
+            </Row>
+          </Col>
+        </Row>
 
-      </div>
+      </Container>
     )
   }
 }
