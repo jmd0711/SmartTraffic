47f2b14dbb5105a6e8625b4ae2f9a6c31d3e33c8
jmd0711 <2jmd0711@gmail.com>
2024-04-20 00:22:58 -0700

    format

--- frontend/smart-traffic/src/components/routes/privateroutes.jsx
+++ frontend/smart-traffic/src/components/routes/privateroutes.jsx
@@ -1,9 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom"
 
 const PrivateRoutes = () => {
-    return (
-        localStorage.userToken ? <Outlet/> : <Navigate to="/login"/>
-    )
+  return (
+    localStorage.userToken ? <Outlet/> : <Navigate to="/login"/>
+  )
 }
 
 export default PrivateRoutes
