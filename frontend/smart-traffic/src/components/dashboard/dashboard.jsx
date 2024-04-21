e70bf496c60ce01b7839bb36a7b457ded034b0fb
jmd0711 <2jmd0711@gmail.com>
2024-04-21 01:12:15 -0700

    added map

--- frontend/smart-traffic/src/components/dashboard/dashboard.jsx
+++ frontend/smart-traffic/src/components/dashboard/dashboard.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react"
 import { Container, Row, Col, Stack, InputGroup, Form, Button } from "react-bootstrap"
 import { Component } from "react"
 import { withRouter } from "../withrouter"
-
+import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
 import "./dashboard.css"
+import * as L from "leaflet";
 
 class Dashboard extends Component {
   constructor() {
@@ -15,23 +16,23 @@ class Dashboard extends Component {
       <div className="main-page">
         <Container>
           {/*<h3 className="page-header"> DASHBOARD PAGE </h3>*/}
-          <Row className="justify-content-center">
+          <Row className="justify-content-center page-container">
 
             <Col xs={4} className="section sidebar">
               <h1>Incidents</h1>
-                <Form.Control
-                  placeholder="Filter"
-                  aria-label="Filter"
-                  aria-describedby="basic-addon2"
-                />
+              <Form.Control
+                placeholder="Filter"
+                aria-label="Filter"
+                aria-describedby="basic-addon2"
+              />
               <Stack gap={3}>
                 <div className="p-2">First item</div>
                 <div className="p-2">Second item</div>
                 <div className="p-2">Third item</div>
               </Stack>
             </Col>
-            <Col>
-              <Row className="section map justify-content-center">
+            <Col className="d-flex flex-column">
+              <Row className="section map justify-content-center p-2">
                 <InputGroup className="mb-3">
                   <Form.Control
                     placeholder="Search"
@@ -41,10 +42,15 @@ class Dashboard extends Component {
                   <Button variant="primary" id="button-addon2">
                     Search
                   </Button>
+                  <MapContainer center={[37.7749, -122.4194]} zoom={12} scrollWheelZoom={false}>
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                  </MapContainer>
                 </InputGroup>
-                Map
               </Row>
-              <Row className="section">
+              <Row className="section p-2">
                 <Stack direction="horizontal" gap={3} className="justify-content-md-center">
                   <div className="p-2">First item</div>
                   <div className="p-2">Second item</div>
@@ -55,6 +61,7 @@ class Dashboard extends Component {
           </Row>
 
         </Container>
+
       </div>
     )
   }
