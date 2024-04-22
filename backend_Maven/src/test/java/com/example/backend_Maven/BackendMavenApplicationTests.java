655bab5ecf2b83358a29324035b5d2129722c6b2
anamatxcova <anamatxcova@gmail.com>
2024-04-21 23:06:54 -0700

    changed the backend configuration

---
+++ backend_Maven/src/test/java/com/example/backend_Maven/BackendMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_Maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
