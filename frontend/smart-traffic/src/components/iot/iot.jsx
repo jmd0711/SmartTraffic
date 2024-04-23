bcd884d0adbd677ccd4cdfcaa8ccedc53adfc155
jmd0711 <2jmd0711@gmail.com>
2024-04-22 19:39:55 -0700

    adjust map size

--- frontend/smart-traffic/src/components/iot/iot.jsx
+++ frontend/smart-traffic/src/components/iot/iot.jsx
@@ -52,7 +52,7 @@ class Drone extends Component {
               />
               <Button variant="primary" type="submit">Search</Button>
             </Form>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '65%', width: '100%' }}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
