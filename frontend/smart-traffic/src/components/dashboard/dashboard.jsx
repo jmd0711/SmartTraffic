6e0fc8483e905e6c52da34a51a7955eaa7ad23f5
jmd0711 <2jmd0711@gmail.com>
2024-04-24 01:33:44 -0700

    search by address/location

--- frontend/smart-traffic/src/components/dashboard/dashboard.jsx
+++ frontend/smart-traffic/src/components/dashboard/dashboard.jsx
@@ -2,13 +2,15 @@ import 'leaflet/dist/leaflet.css';
 import '../components.css';
 import React, { Component, useEffect, useState } from 'react';
 import { withRouter } from "../withrouter";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import L from 'leaflet';
 import BarChart from './barchart'
 import LineChart from './linechart';
 import RadialBarChart from './radialbar'
 import StreamChart from './streamchart'
+import '../geocodeapi';
+import { fromAddress } from 'react-geocode';
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
@@ -17,11 +19,34 @@ L.Icon.Default.mergeOptions({
 });
 
 class Dashboard extends Component {
+  constructor() {
+    super()
+    this.mapRef = React.createRef();
+    this.state = {
+      search: '',
+      error: {}
+    }
+    
+    this.onChange = this.onChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
 
-  handleSearch = (event) => {
-    event.preventDefault();
-    console.log("Search submitted");
+  }
+  handleSearch = (e) => {
+    e.preventDefault();
+    //console.log(this.state.search);
+
+    fromAddress(this.state.search)
+    .then(({ results }) => {
+      const { lat, lng } = results[0].geometry.location
+      const map = this.mapRef.current
+      map.setView([lat, lng], map.getZoom())
+    })
+    .catch(console.error)
   };
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
   render() {
     const position = [37.3346, -121.8753]; // Example position for San Jose
@@ -46,13 +71,16 @@ class Dashboard extends Component {
             <Form className="d-flex mb-3" onSubmit={this.handleSearch}>
               <Form.Control
                 type="text"
+                name="search"
                 placeholder="Search Area"
+                value={this.state.search}
+                onChange={this.onChange}
                 className="me-3"
                 style={{ flexGrow: 1 }}
               />
               <Button variant="primary" type="submit">Search</Button>
             </Form>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false} ref={this.mapRef}>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
---
+++ frontend/smart-traffic/src/components/geocodeapi.jsx
@@ -0,0 +1,10 @@
+import {
+    setDefaults,
+} from "react-geocode";
+
+setDefaults({
+    key: "######", // Your API key here.
+    language: "en", // Default language for responses.
+    region: "es", // Default region for responses.
+});
+
