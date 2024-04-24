cf9f2f27f9fec0b3b32bd94166115e5d76f026f2
Jasper Matthew Dumdumaya <35779500+jmd0711@users.noreply.github.com>
2024-04-24 03:41:18 -0700

    Update geocodeapi.jsx

--- frontend/smart-traffic/src/components/geocodeapi.jsx
+++ frontend/smart-traffic/src/components/geocodeapi.jsx
@@ -3,7 +3,7 @@ import {
 } from "react-geocode";
 
 setDefaults({
-    key: "AIzaSyAXpVQxx1o3LpuMMw0CO8mK90KNG-LaKUE", // Your API key here.
+    key: "####", // Your API key here.
     language: "en", // Default language for responses.
     region: "es", // Default region for responses.
 });
