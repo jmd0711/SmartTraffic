74048c0b74538dc505f16bc68f18c52de863bb14
jmd0711 <2jmd0711@gmail.com>
2024-04-23 22:17:38 -0700

    temporary workaround for html status bug

--- frontend/smart-traffic/src/components/user/userapi.jsx
+++ frontend/smart-traffic/src/components/user/userapi.jsx
@@ -3,27 +3,36 @@ import axios from "axios"
 const apiURL = "http://localhost:8080/"
 
 export const signup = (newUser) => {
-    return axios
-        .post(apiURL + 'user/signup', newUser)
-        .then((res) => {
-            window.alert("You've successfully signed up.")
-            localStorage.setItem("userToken", JSON.stringify(res.data.data))
-            return res.data.data
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+  return axios
+    .post(apiURL + 'user/signup', newUser)
+    .then((res) => {
+      // Currently, response is wrapped in another response so have to check error within first success
+      if (res.data.status == "200") {
+        window.alert("You've successfully signed up.")
+        localStorage.setItem("userToken", JSON.stringify(res.data.data))
+        return res.data.data
+      } else {
+        console.log(res.data)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 export const login = (user) => {
-    return axios
-      .post(apiURL + 'user/login', user)
-      .then((res) => {
-        localStorage.setItem("userToken", JSON.stringify(res.data.data));
-        return res.data.data;
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  };
-  
+  return axios
+    .post(apiURL + 'user/login', user)
+    .then((res) => {
+      // Currently, response is wrapped in another response so have to check error within first success
+      if (res.data.status == "200") {
+      localStorage.setItem("userToken", JSON.stringify(res.data.data));
+      return res.data.data;
+      } else {
+        console.log(res.data)
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+};
